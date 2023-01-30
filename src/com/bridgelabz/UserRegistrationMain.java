ebfdfab4f26f87abc9430a63f67bc95ab5fb6275
Rupali Anap <anaprupali333@gmail.com>
2023-01-30 09:52:48 -0800

    Added: Valid Numeric Password

--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -3,40 +3,43 @@ import java.util.Scanner;
 
 public class UserRegistrationMain {
 
-        public static void main(String[] args) {
-            System.out.println("Welcome to User Registration Program");
+    public static void main(String[] args) {
 
-            UserRegistration user = new UserRegistration();
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter 1: To validate First Name:\nEnter 2: To validate Last Name:\nEnter 3: validate email:\nEnter 4: validate mobile number:" +
-                    "\nEnter 5: validate password:\nEnter 6: validate oneUpperCase password: ");
+        System.out.println("Welcome to User Registration Program");
 
-            switch (sc.nextInt()) {
-                case 1:
-                    user.validFirstName();
-                    break;
-                case 2:
-                    user.validLastName();
-                    break;
-                case 3:
-                    user.validEmail();
-                    break;
-                case 4:
-                    user.validMobileNumber();
-                    break;
-                case 5:
-                    user.validPassword();
-                    break;
-                case 6:
-                    user.validUpperCasePassword();
-                    break;
-            }
+        UserRegistration user = new UserRegistration();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1: To validate First Name:\nEnter 2: To validate Last Name:\nEnter 3: validate email:\nEnter 4: validate mobile number:" +
+                "\nEnter 5: validate password:\nEnter 6: validate oneUpperCase password:\nEnter 7: validate numericPassword: ");
+
+        switch (sc.nextInt()) {
+            case 1:
+                user.validFirstName();
+                break;
+            case 2:
+                user.validLastName();
+                break;
+            case 3:
+                user.validEmail();
+                break;
+            case 4:
+                user.validMobileNumber();
+                break;
+            case 5:
+                user.validPassword();
+                break;
+            case 6:
+                user.validUpperCasePassword();
+                break;
+            case 7:
+
+                user.validNumericPassword();
+                break;
         }
+
+    }
+
 }
-
-
-
-
 
 
 
