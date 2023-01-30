c09b8bf8cc44112018789dc95255c9cc48108623
Rupali Anap <anaprupali333@gmail.com>
2023-01-30 09:46:36 -0800

    Added: Valid Upper case Password

--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class UserRegistrationMain {
 
         public static void main(String[] args) {
-
             System.out.println("Welcome to User Registration Program");
 
             UserRegistration user = new UserRegistration();
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter 1: To validate First Name:\nEnter 2: To validate Last Name:\nEnter 3: validate email:\nEnter 4: validate mobile number:\nEnter 5: validate password: ");
+            System.out.println("Enter 1: To validate First Name:\nEnter 2: To validate Last Name:\nEnter 3: validate email:\nEnter 4: validate mobile number:" +
+                    "\nEnter 5: validate password:\nEnter 6: validate oneUpperCase password: ");
 
             switch (sc.nextInt()) {
                 case 1:
@@ -26,6 +26,9 @@ public class UserRegistrationMain {
                     break;
                 case 5:
                     user.validPassword();
+                    break;
+                case 6:
+                    user.validUpperCasePassword();
                     break;
             }
         }
